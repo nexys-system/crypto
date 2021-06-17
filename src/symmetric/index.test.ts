c61b9bb1203c8d26bd13680fa8231a01ec36fe13
John B <johan@nexys.ch>
2021-06-17 15:03:12 +0200

    Update index.test.ts

--- src/symmetric/index.test.ts
+++ src/symmetric/index.test.ts
@@ -5,6 +5,7 @@ test("encrypt and decrypt", () => {
   const secretKey = "durbdhrbserjvcejg37fg3hcishfjkic"; // key must be 32 bytes for aes256
 
   const e = C.encrypt(text, secretKey);
+  console.log(e);
   const d = C.decrypt(e, secretKey);
 
   expect(text).toEqual(d);
